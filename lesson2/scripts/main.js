1ed11173c3c583a3f79bb590f0b99b9fca7971a8
JaxonAtBYUI <jaxonhamm@byui.edu>
2022-05-07 03:42:37 -0600

    Made the Shire Chamber of Commerce Site

--- lesson2/scripts/main.js
+++
@@ -1,6 +0,0 @@
-// Replace the copyright date with the current year.
-let d = new Date();
-document.getElementById('year').textContent = d.getFullYear();
-
-// Replace the last modified date with the last date modified.
-document.getElementById('date').textContent = document.lastModified;
